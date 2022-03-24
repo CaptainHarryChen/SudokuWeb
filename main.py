0e49338ba9fee2cd295a64cbb54e81513b07c5a6
Captain Chen <2429643516@qq.com>
2022-03-24 20:22:04 +0800

    添加录入数据功能；添加提交按钮；TODO：表单提交未完成

--- main.py
+++ main.py
@@ -69,5 +69,48 @@ def sudoku():
     return render_template("sudoku.html", placeContent=tabSudoku)
 
 
+
+@sudoku_web.route("/sudoku_result.html",methods=["post"])
+def result():
+    pass
+
+
+@sudoku_web.route("/inputsudo.html")
+def input_sudo():
+    dataSudoku = ["0" for i in range(81)]
+
+    tabSudoku = "<table cellpadding='0' cellspacing='0' border='1'>"
+    for i in range(0, 9):
+        tabSudoku += "<tr>"
+        for j in range(0, 9):
+            cellNum = dataSudoku[i*9+j]
+            zeroState = rightState = bottomState = False
+            if cellNum == "0":
+                cellNum = ""
+                zeroState = True
+            cellDiv = f"<div class='input_cell' id='input_cell_{i}{j}' contenteditable='{zeroState}' style='max-width:10mm max-height:10mm'>{cellNum}</div>"
+
+            if j % 3 == 2 and j != 8:
+                rightState = True
+            if i % 3 == 2 and i != 8:
+                bottomState = True
+
+            classValue = ""
+            if zeroState == True:
+                classValue += " zeroCell"
+            if rightState == True:
+                classValue += " rightBolder"
+            if bottomState == True:
+                classValue += " bottomBolder"
+            classValue = classValue.strip()
+            tdType = "<td>"
+            if len(classValue) != 0:
+                tdType = f"<td class='{classValue}'>"
+            tabSudoku += f"{tdType}{cellDiv}</td>"
+        tabSudoku += "</tr>"
+    tabSudoku += "</table>"
+    return render_template("input_sudo.html", placeContent=tabSudoku)
+
+
 if __name__ == "__main__":
     sudoku_web.run(host="0.0.0.0", port=80, debug=True)
